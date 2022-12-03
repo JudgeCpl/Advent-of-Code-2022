0a291939b80929c90d032be33610e1039380041e
JudgeCpl <djaldridge@hotmail.co.uk>
2022-12-03 17:25:14 +0000

    Day 3 defeated

--- Day-3/Day-3.py
+++ Day-3/Day-3.py
@@ -2,18 +2,31 @@ import os
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 inputList = []
-cnt=0
 with open(os.path.join(__location__, 'Day-3-Input.txt')) as stratergy:
     for line in stratergy:
-        cnt+=1
         newLine = line.translate({ord("\n"):None})
         length = int(len(newLine) / 2)
         inputList.append([newLine[i:i+length] for i in range(0, len(newLine), length)])
 
+list = [['a'], ['b'], ['c'], ['d'], ['e'], ['f'], ['g'], ['h'], ['i'], ['j'], ['k'], ['l'], ['m'], ['n'], ['o'], ['p'], ['q'], ['r'], ['s'], ['t'], ['u'], ['v'], ['w'], ['x'], ['y'], ['z'], ['A'], ['B'], ['C'], ['D'], ['E'], ['F'], ['G'], ['H'], ['I'], ['J'], ['K'], ['L'], ['M'], ['N'], ['O'], ['P'], ['Q'], ['R'], ['S'], ['T'], ['U'], ['V'], ['W'], ['X'], ['Y'], ['Z']]
 cnt = 0
+for i in list:
+    cnt += 1
+    list[cnt-1].append(cnt)
+
+def findSumPriorities(sumList):
+    sumOfPriorities = 0
+    for i in sumList:
+        for e in list:
+           if i == e[0]:
+              sumOfPriorities += e[1]
+              break
+
+    print("The sum of priorities is", sumOfPriorities)
+
+"""
 sharedItemsList = []
 for i in inputList:
-    cnt+=1
     letterFound = False
     for e in i[0]:
         if letterFound:
@@ -23,19 +36,30 @@ for i in inputList:
                 sharedItemsList.append(e)
                 letterFound = True
                 break
+findSumPriorities(sharedItemList)
+"""
 
-list = [['a'], ['b'], ['c'], ['d'], ['e'], ['f'], ['g'], ['h'], ['i'], ['j'], ['k'], ['l'], ['m'], ['n'], ['o'], ['p'], ['q'], ['r'], ['s'], ['t'], ['u'], ['v'], ['w'], ['x'], ['y'], ['z'], ['A'], ['B'], ['C'], ['D'], ['E'], ['F'], ['G'], ['H'], ['I'], ['J'], ['K'], ['L'], ['M'], ['N'], ['O'], ['P'], ['Q'], ['R'], ['S'], ['T'], ['U'], ['V'], ['W'], ['X'], ['Y'], ['Z']]
-cnt = 0
-for i in list:
-    cnt += 1
-    list[cnt-1].append(cnt)
+teamSharedItemList = []
+count = 0
+while len(inputList) > 0:
+    count += 1
+    teamList = []
+    cnt = 0
+    while cnt < 3:
+        teamList.append(inputList[0][0] + inputList[0][1])
+        inputList.pop(0)
+        cnt += 1
 
-sumOfPriorities = 0
-for i in sharedItemsList:
-    for e in list:
-        if i == e[0]:
-            sumOfPriorities += e[1]
+    letterFound = False
+    for i in teamList[0]:
+        if letterFound == True:
             break
-
-print(sharedItemsList[0], sharedItemsList[1], sharedItemsList[2])
-print("The sum of priorities is", sumOfPriorities)
+        for o in teamList[1]:
+            if letterFound == True:
+                break
+            for p in teamList[2]:
+                if i == o == p:
+                    teamSharedItemList.append(o)
+                    letterFound = True
+                    break
+findSumPriorities(teamSharedItemList)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Advent-of-Code-2022
+I tried to python. Sorry for my crimes against coding.
