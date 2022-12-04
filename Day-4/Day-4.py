cf99a4f99a4440a4fde759fcc0f5b3c7cd4dc4bf
JudgeCpl <djaldridge@hotmail.co.uk>
2022-12-04 15:20:51 +0000

    Day 4 part 2 perished :D

--- Day-4/Day-4.py
+++ Day-4/Day-4.py
@@ -11,18 +11,19 @@ with open(os.path.join(__location__, 'Day-4-Input.txt')) as assignments:
             append.append(i.split("-"))
         inputList.append(append)
 
-cnt = 0
+encompass = 0
+overlap = 0
 for e in inputList:
     one1 = int(e[0][0])
     one2 = int(e[0][1])
     two1 = int(e[1][0])
     two2 = int(e[1][1])
 
-    if (one1<=two1) and (one2>=two2):
-        cnt += 1
-        print(e[0], "contains", e[1])
-    elif (two1<=one1) and (two2>=one2):
-        cnt += 1
-        print(e[1], "contains", e[0])
+    if ((one1<=two1) and (one2>=two2)) or ((two1<=one1) and (two2>=one2)):
+        encompass += 1
+        overlap += 1
+    elif ((one1<=two1) and (two1<=one2)) or ((one1<=two2) and (two2<=one2)) or ((two1<=one1) and (one1<=two2)) or ((two1<=one2) and (one2<=two2)):
+        overlap += 1
 
-print(cnt, "pairs have an assignment that encompesses the other")
+print(encompass, "pairs have an assignment that encompesses the other. ")
+print(overlap, "assignment pairs overlap. ")
