60424a5994e1917a1ab55ce4804f7a50ad632f9a
JudgeCpl <djaldridge@hotmail.co.uk>
2022-12-07 22:36:03 +0000

    Day 7 smashed :D

--- Day-7/Day-7.py
+++ Day-7/Day-7.py
@@ -23,8 +23,9 @@ def cntSize(path):
         elif (i.__contains__("."))==False:
             e = os.path.join(path, i)
             eE = os.path.join(e, "count.txt")
-            fCnt = open(eE, "r").readlines()
-            cnt += int(fCnt[0])
+            if os.path.exists(eE):
+                fCnt = open(eE, "r").readlines()
+                cnt += int(fCnt[0])
     
     file = os.path.join(path, "count.txt")
     if os.path.exists(file)==False:
@@ -35,11 +36,7 @@ def cntSize(path):
         cntFile = open(file, "w")
         cntFile.write(str(cnt))
         cntFile.close
-
-    if cnt < 100000:
-        return cnt
-    else:
-        return 0
+    return cnt
 
 for line in input:
     line = line.strip("\n")
@@ -65,11 +62,33 @@ for line in input:
     if line[0]=="$":
         if line=="$ cd /":
             path = os.path.join(path, "Dir")
-            total += cntSize(path)
+            c = cntSize(path)
+            if c <100000:
+                total += c
         elif line=="$ cd ..":
             path = os.path.dirname(path)
         elif line.__contains__("cd"):
             path = os.path.join(path, line[5::])
-            total += cntSize(path)
+            c = cntSize(path)
+            if c <100000:
+                total += c
+print("The sum of directories smaller than 100000 is:", total)
 
-print(total)
+result = 70000000
+path = ""
+for line in input:
+    line = line.strip("\n")
+    if line[0]=="$":
+        if line=="$ cd /":
+            path = os.path.join(path, "Dir")
+            c = cntSize(path)
+            if c>2558312 and c<result:
+                result = c
+        elif line=="$ cd ..":
+            path = os.path.dirname(path)
+        elif line.__contains__("cd"):
+            path = os.path.join(path, line[5::])
+            c = cntSize(path)
+            if c>2558312 and c<result:
+                result = c
+print("The smallest directory to delete is:", result)
